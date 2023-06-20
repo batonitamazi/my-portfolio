fae620aa9fddaea2a22e8dfa3552a6b37f05a1f7
tamaz_mirianashvili <tamazi@DESKTOP-2KFTA3O>
2023-06-20 17:12:41 +0400

    feat: minimal design fix

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <WorkExperience />
         <Contact />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- my-portfolio-app/src/components/about/About.js
+++ my-portfolio-app/src/components/about/About.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
     <section id="about">
-      <h1>About Me</h1>
-      <p>Add information about yourself here.</p>
+      <div className="container left-container">
+        <h4>Tamazi Mirianashvili</h4>
+        <h3>Get to know me</h3>
+        <p>
+          As a passionate programmer and computer science geek, I find immense
+          joy and fulfillment in exploring the vast realm of technology. My
+          journey in the world of programming began early on, captivated by the
+          magic of turning lines of code into functional software and innovative
+          solutions. With a firm foundation in computer science, I have honed my
+          skills to become a versatile and dedicated programmer. My programming
+          expertise encompasses a wide range of languages and technologies. From
+          the elegant simplicity of Python to the robustness of Java, I embrace
+          the diversity of programming languages, always eager to explore and
+          learn new ones. My curiosity extends beyond the surface level, as I
+          dive deep into the core concepts of algorithms, data structures, and
+          software design patterns. Solving complex problems and optimizing code
+          efficiency excite me, driving me to create elegant and scalable
+          solutions.
+        </p>
+        <div className="button-container">
+          <a href="#work">
+            <button>Work Experience</button>
+          </a>
+        </div>
+      </div>
+      <div className="container right-container">
+        <div
+          style={{
+            width: "70%",
+            height: "100%",
+            backgroundColor: "gray",
+            marginRight: "40px",
+          }}
+        ></div>
+        {/* <img src="./assets/my-picture.jpg" alt="Tamaz Mirianashvili Developer" /> */}
+      </div>
     </section>
   );
-}
+};
 
 export default About;
