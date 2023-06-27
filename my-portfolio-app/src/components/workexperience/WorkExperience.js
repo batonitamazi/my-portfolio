01f32a64f07f670091a79f42657db096627bf588
tamaz_mirianashvili <tamazi@DESKTOP-2KFTA3O>
2023-06-27 12:10:00 +0400

    new features animations

--- my-portfolio-app/src/components/workexperience/WorkExperience.js
+++ my-portfolio-app/src/components/workexperience/WorkExperience.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React from "react";
+import "./workExperience.css";
 
 const WorkExperience = () => {
   return (
     <section id="work">
       <h1>Work Experience</h1>
-      <ul>
+      <ul className="working-experience-list">
         <li>
-          <h2>Job Title</h2>
-          <p>Date - Date</p>
-          <p>Description of your work experience.</p>
+          <div className="work-item">
+            <div className="left-column">
+              <img src="./assets/logo.png" alt="Company 1 Logo" />
+              <div className="company-info">
+                <h2>Company 1</h2>
+                <h3>Job Title 1</h3>
+                <p>Date - Date</p>
+                <p>Description of your work experience.</p>
+              </div>
+            </div>
+          </div>
         </li>
-        
+        <li>
+          <div className="work-item second-working-item">
+            <div className="left-column">
+              <div className="company-info">
+                <h2>Company 2</h2>
+                <h3>Job Title 2</h3>
+                <p>Date - Date</p>
+                <p>Description of your work experience.</p>
+              </div>
+              <img src="./assets/logo.png" alt="Company 2 Logo" />
+            </div>
+          </div>
+        </li>
       </ul>
     </section>
   );
-}
+};
 
 export default WorkExperience;
